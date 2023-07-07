023f87c535fb46d64964fb4f72f64ca6544bd910
Faranosh Amini <faranosh.amini4@gmail.com>
2023-07-07 12:28:19 +0430

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,21 @@
 class Group < ApplicationRecord
-  belongs_to :user
-  has_many :entities
-  validates :name, :user, presence: true
-  validates :icon, presence: true
-end
+    belongs_to :user
+    has_many :group_entities, foreign_key: :group_id
+    has_many :entities, through: :group_entities, foreign_key: :group_id
+    validates :name, presence: true
+    def total_amount
+      total = 0
+      entities.each do |t|
+        total += t.amount
+      end
+      total
+    end
+  
+    def date
+      date = ''
+      date += created_at.day.to_s
+      date += (Date::ABBR_MONTHNAMES[created_at.month]).to_s
+      date += created_at.year.to_s
+      date
+    end
+  end
