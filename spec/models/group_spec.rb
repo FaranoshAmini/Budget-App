9d8831c66514210f2d12d325bea2d172cea39033
Faranosh Amini <faranosh.amini4@gmail.com>
2023-07-07 16:55:26 +0430

    Create the test for group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'GET /index' do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
 end
