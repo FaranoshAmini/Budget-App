82d9d3a1d65329a933e7d0b099ebd58becabca40
Faranosh Amini <faranosh.amini4@gmail.com>
2023-07-07 19:15:17 +0430

    Fix rubocop errors

--- app/models/group_entity.rb
+++ app/models/group_entity.rb
@@ -1,6 +1,5 @@
 class GroupEntity < ApplicationRecord
-    belongs_to :group
-    belongs_to :entity
-    validates :entity, uniqueness: { scope: :group, message: 'already exists in this category' }
-  end
-  
+  belongs_to :group
+  belongs_to :entity
+  validates :entity, uniqueness: { scope: :group, message: 'already exists in this category' }
+end
