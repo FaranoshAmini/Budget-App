6d483422b9bdc87f2feb3946a7004454b850a6f2
Faranosh Amini <faranosh.amini4@gmail.com>
2023-07-07 14:25:44 +0430

    Update entity.rb

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,13 +1,13 @@
 class Entity < ApplicationRecord
-    has_many :group_entities, foreign_key: :entity_id
-    has_many :groups, through: :group_entities, foreign_key: :entity_id
-    validates :name, :author, :group, :amount, presence: true
-  
-    def date
-      date = ''
-      date += "#{created_at.day} "
-      date += "#{Date::ABBR_MONTHNAMES[created_at.month]} "
-      date += created_at.year.to_s
-      date
-    end
+  has_many :group_entities, foreign_key: :entity_id
+  has_many :groups, through: :group_entities, foreign_key: :entity_id
+  validates :name, :author, :group, :amount, presence: true
+
+  def date
+    date = ''
+    date += "#{created_at.day} "
+    date += "#{Date::ABBR_MONTHNAMES[created_at.month]} "
+    date += created_at.year.to_s
+    date
   end
+end
