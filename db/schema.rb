9beaeae6eb64d1fb79d37cf8848e90cdb7b1ccbd
Faranosh Amini <faranosh.amini4@gmail.com>
2023-07-08 23:42:13 +0430

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_183242) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_182738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_183242) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
   end
 
   create_table "entities_groups", id: false, force: :cascade do |t|
@@ -49,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_183242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "entities_groups", "entities"
   add_foreign_key "entities_groups", "groups"
   add_foreign_key "groups", "users", column: "author_id"
