411ea86acdd73c489db96879b719985bf83d9811
Faranosh Amini <faranosh.amini4@gmail.com>
2023-07-15 15:52:27 +0430

    Update the models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,7 +2,7 @@ class Entity < ApplicationRecord
   has_many :group_entities, foreign_key: :entity_id
   belongs_to :author, class_name: 'User'
   has_many :groups, through: :group_entities, foreign_key: :entity_id
-  validates :name, :author, :group, :amount, presence: true
+  validates :name, :author, :groups, :amount, presence: true
 
   def date
     date = ''
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   # has_many :group_entities, foreign_key: :group_id
-  # has_many :entities, through: :group_entities, foreign_key: :group_id
+  has_many :entities, through: :group_entities, foreign_key: :group_id
   # has_many :entities, foreign_key: :group_id
   has_and_belongs_to_many :entities, foreign_key: :group_id, dependent: :destroy
 
